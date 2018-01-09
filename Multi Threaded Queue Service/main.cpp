17aa474a815357b7349bdd16400b56e79d020eeb
Tim Madorma <tmadorma@gmail.com>
2018-01-09 18:22:50 -0500

    Initial Commit

---
+++ Multi Threaded Queue Service/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Multi Threaded Queue Service
+//
+//  Created by Tim Madorma on 1/9/18.
+//  Copyright © 2018 Tim Madorma. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
